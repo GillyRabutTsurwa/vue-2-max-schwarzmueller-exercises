9829f67b534d235eb8d4a36e39bc58c9e912ec89
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-06-05 13:39:58 -0500

    Je sais pas honnetement

--- src/components/Prompt.vue
+++ src/components/Prompt.vue
@@ -8,16 +8,20 @@
       </strong> more about VueJS. Also, I am refreshing my fundamental skills using Vue2, but anything that is not in Vue3, I will skip those altogether for now
     </p>
     <ol>
-      <li>Start the Effect with the Button. The Effect should alternate the "highlight" or "shrink" class on each new setInterval tick (attach respective class to the div with id "effect" below)</li>
-      <li>Create a couple of CSS classes and attach them via the array syntax</li>
-      <li>Let the user enter a class (create some example classes) and attach it</li>
-      <li>Let the user enter a class and enter true/ false for another class (create some example classes) and attach the classes</li>
-      <li>Repeat #3 but now with values for styles (instead of class names). Attach the respective styles. NOTE: Successful with colour, will add other styles in the future</li>
+      <li>Hook up the button to toggle the display of the two paragraphs. Use both v-if and v-show and inspect the elements to see the difference</li>
+      <li>Output an unordered list of array elements of your choice. Also print the index of each element</li>
       <li>
-        Create a simple progress bar with setInterval and style bindings. Start it by hitting the below button.
+        Print all key-value pairs of the following object:
         <strong>
-          <em>Not yet attempted</em>
+          <em>{title: "Bible", author: "God", books: 66}</em>. Also print the index of each item
         </strong>
+      </li>
+      <li>
+        Print the following object (only the values) and also create a nested loop for the array:
+        <strong>
+          <em>{name: 'TESTOBJECT', data: [1.67, 1.33, 0.98, 2.21]}</em>
+        </strong>
+        (hint: use v-for and v-if to achieve this)
       </li>
     </ol>
   </div>
@@ -28,7 +32,7 @@ export default {
   name: "Prompt",
   data() {
     return {
-      title: "Styling"
+      title: "Conditionals & Loops with Lists"
     };
   }
 };
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,31 +1,28 @@
 <template>
   <div>
-    <!-- NOTE: The value of a bound class (like toggleClass) is ALWAYS an object. The property is the name of the class and the value is a true or false values that determines if the class will be implemented. This object can be made inline or in the JavaScript. -->
-    <div v-bind:class="toggleClass">
-      <p>Pinchi ma pinchi</p>
+    <div>
+      <button @click="isShowing = !isShowing" class="btn">Show First</button>
+      <button @click="isShowing2 = !isShowing2" class="btn">Show Second</button>
+      <p v-if="isShowing">Toggle Me</p>
+      <p v-if="isShowing2">Toggle me as well... But how am I different?</p>
     </div>
-    <button @click="startEffect" class="btn">Start Effect</button>
-    <!-- NOTE: Okay so what I said above is not necessarily true. Je veux dire que: we can also use arrays. In this case, the value of the bound class (arrayClasses) is an array of the classes (which are strings) defined in the CSS -->
-    <p v-bind:class="arrayClasses">Array of classes pinchi</p>
-    <!-- NOTE: Voilà ce que j'ai fait...
-    Each time we press enter on the input, we run the addClass() which addes a class to the array inputArrayClasses. 
-    We use attach the variable addClassName to be the value of the input using two-way binding and add this value to the inputArrayClasses, again, using the addClass()-->
-    <input
-      type="text"
-      @keyup.enter="addClass"
-      v-model="addedClassName"
-      placeholder="Enter Class Names"
-    />
-    <p>{{addedClassName}}</p>
-    <h4 v-bind:class="inputArrayClasses">Les classes m'ajoutent via l'entrée dessûs (input)</h4>
-    <!--  -->
-    <input
-      type="text"
-      @keyup.enter="addColour"
-      v-model="inputColour"
-      placeholder="Enter Colour Names"
-    />
-    <h4 v-bind:style="colourObj">Les styles m'ajoutent via l'entrée dessûs ma pinchi</h4>
+    <div>
+      <h4>List of CSS & JS Frameworks</h4>
+      <ul>
+        <li
+          v-for="(currentFramework, index) in frameworks"
+          v-bind:key="index"
+        >{{currentFramework}} - {{index + 1}}</li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li v-for="(currentValue, propertyName, index) in myObject" v-bind:key="currentValue.id">
+          <strong>{{index + 1}}</strong>
+          - {{propertyName}}: {{currentValue}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -33,41 +30,35 @@
 export default {
   data() {
     return {
-      // Toggle class is the object declared in the template
-      toggleClass: {
-        // Highlight and shrink are CSS classnames, go to CSS for proof;
-        highlight: true,
-        shrink: false
+      isShowing: false,
+      isShowing2: false,
+      frameworks: [
+        "Vue",
+        "Svelte",
+        "React",
+        "Angular",
+        "Bulma",
+        "Bootstrap",
+        "Materialize",
+        "UI-Kit",
+        "Semantic UI"
+      ],
+      array: ["Max", "Anna", "Chris", "Manu"],
+      myObject: {
+        title: "Bible",
+        author: "God",
+        books: "66",
+        // TESTING:
+        id: 1
       },
-      // Array of classes. Classes are written as strings.
-      arrayClasses: ["arrayClass1", "arrayClass2", "arrayClass3"],
-      // variable responsible for adding classnames to the inputArrayClasses array.
-      addedClassName: "",
-      // array of classes. Classes are added using the input value addClassName
-      inputArrayClasses: [],
-      inputColour: "",
-      /////////////////////////////////////
-      colourObj: {
-        color: ""
+      testData: {
+        name: "TESTOBJECT",
+        id: 10,
+        data: [1.67, 1.33, 0.98, 2.21]
       }
     };
   },
-  methods: {
-    // This method just toggles the classes between each other;
-    startEffect() {
-      setInterval(() => {
-        this.toggleClass.highlight = !this.toggleClass.highlight;
-        this.toggleClass.shrink = !this.toggleClass.shrink;
-      }, 3000);
-    },
-    addClass() {
-      if (this.addedClassName === "") this.inputArrayClasses = [];
-      this.inputArrayClasses = [...this.inputArrayClasses, this.addedClassName];
-    },
-    addColour() {
-      this.colourObj.color = this.inputColour;
-    }
-  }
+  methods: {}
 };
 </script>
 
@@ -76,25 +67,5 @@ export default {
   width: 100px;
   height: 100px;
   border: 1px solid black;
-}
-
-.highlight {
-  background-color: red;
-  width: 200px !important;
-}
-
-.shrink {
-  background-color: gray;
-  width: 50px !important;
-}
-
-.arrayClass1 {
-  color: forestgreen;
-}
-.arrayClass2 {
-  text-transform: uppercase;
-}
-.arrayClass3 {
-  font-size: 2em;
 }
 </style>
